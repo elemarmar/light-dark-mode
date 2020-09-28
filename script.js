d78f565851b6cae137617f02836369aac8c9babb
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-09-28 17:15:11 +0200

    Update script

--- script.js
+++ script.js
@@ -1,13 +1,61 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+const DARK_THEME = 'dark';
+const LIGHT_THEME = 'light';
+
+// Dark or Light Images
+function imageMode(color) {
+  image1.src = `img/undraw_proud_coder_${color}.svg`;
+  image2.src = `img/undraw_feeling_proud_${color}.svg`;
+  image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+function toggleDarkLightMode(mode) {
+  nav.style.backgroundColor =
+    mode === DARK_THEME ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor =
+    mode === DARK_THEME ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+  // Icon text
+  toggleIcon.children[0].textContent =
+    mode === DARK_THEME ? 'Dark Mode' : 'Light Mode';
+  // Change icon
+  mode === DARK_THEME
+    ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
+    : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+
+  // Changing images
+  mode === DARK_THEME ? imageMode('dark') : imageMode('light');
+}
 
 // Switch Theme Dynamically
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+    toggleDarkLightMode(DARK_THEME);
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+    toggleDarkLightMode(LIGHT_THEME);
   }
 }
 
 // Event Listeners
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    toggleDarkLightMode(DARK_THEME);
+  }
+}
